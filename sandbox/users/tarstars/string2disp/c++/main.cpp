4f572edeaee6602ae624fedeb8c78086a4793b20
Arseniy Trushin <142857@mail.ru>
2017-01-30 00:14:26 +0300

    struggle with optimal bitfield started

--- sandbox/users/tarstars/string2disp/c++/main.cpp
+++ sandbox/users/tarstars/string2disp/c++/main.cpp
@@ -16,6 +16,7 @@ public:
       current_byte &=  ~mask;
     }
   }
+  unsigned char * GetBuffer() {return &bytes[0];}
 private:
   size_t h, w;
   std::vector<unsigned char> bytes;
@@ -52,6 +53,46 @@ SaveAsDotMinus(std::ostream& os, const BitMatrix& bm) {
   return os;
 }
 
+void
+BruteForceFill(BitMatrix *buffer, int ltx, int lty, int rw, int rh, bool set_to_one) {
+  for (int p = lty; p < lty + rh; ++p) {
+    for (int q = ltx; q < ltx + rw; ++q) {
+      buffer->Set(p, q, set_to_one);
+    }
+  }
+}
+
+void
+NiceFill(unsigned char *buffer, int w, int ltx, int lty, int rw, int rh, bool filler) {
+  if (ltx%8 != 0) {
+    unsigned char left_mask = 0;
+    for (int p = std::max(0, ltx % 8 - rw); p < ltx % 8; --p) {
+      left_mask |= 128 >> p;
+    }
+    for (int p = lty; p <= lty + rh; ++p) {
+      if (filler) {
+	buffer[p*w + ltx/8] |= left_mask;
+      } else {
+	buffer[p*w + ltx/8] &= ~left_mask;
+      }
+    }
+  }
+  if ((ltx + rw) %8 != 0 && ltx / 8 != (ltx + rw) / 8) {
+    unsigned char right_mask = 0;
+    for (int p = ltx % 8; p > std::max(-1, ltx % 8 - rw); --p) {
+      right_mask |= 128 >> p;
+    }
+    for (int p = lty; p <= lty + rh; ++p) {
+      if (filler) {
+	buffer[p*w + ltx/8] |= right_mask;
+      } else {
+	buffer[p*w + ltx/8] &= ~right_mask;
+      }
+    }
+  }
+
+}
+
 int main() {
   size_t h, w;
   int ltx, lty, rh, rw;
@@ -60,11 +101,7 @@ int main() {
   std::cin >> ltx >> lty >> rw >> rh;
   std::cin >> set_to_it;
   BitMatrix BitField = LoadFromDotMinus(h, w, std::cin);
-  for (int p = lty; p < lty + rh; ++p) {
-    for (int q = ltx; q < ltx + rw; ++q) {
-      BitField.Set(p, q, set_to_it==1);
-    }
-  }
+  NiceFill(BitField.GetBuffer(), BitField.Width(), ltx, lty, rw, rh, set_to_it == 1);
   SaveAsDotMinus(std::cout, BitField);
   return 0;
 }
