74adcb27727c8f028ff94d8df477afaefb9e0ca8
Arseniy Trushin <142857@mail.ru>
2017-06-05 15:31:19 +0300

    kmp works

--- sandbox/users/tarstars/kmp/main.py
+++ sandbox/users/tarstars/kmp/main.py
@@ -9,15 +9,31 @@ def fibb_str(n):
         a, b = b, a + b
     return a
 
-def init_kmp(p):
+
+def init_kmp(pat):
     pos = -1
-    f = [pos] * len(p)
-    for ind in range(1, len(p)):
-        while pos >= 0 and p[pos] != p[ind]:
+    f = [pos] * len(pat)
+    for ind in range(1, len(pat)):
+        while pos >= 0 and pat[pos] != pat[ind]:
             pos = f[pos]
         pos = pos + 1
         f[ind] = pos
-    return (str(p), f)
+    return (str(pat), f)
+
+
+def apply_kmp(pat, f, s):
+    pos = 0
+    ans = []
+    for ind in range(len(s)):
+        while pos >= 0 and pat[pos] != s[ind]:
+            pos = f[pos]
+        pos = pos + 1
+        if pos == len(pat):
+            ans.append(ind - len(pat) + 1)
+            pos = pos - 1
+            while pos >= 0 and pat[pos] != s[ind]:
+                pos = f[pos]
+    return ans
 
 
 class TestInitKMP(unittest.TestCase):
@@ -49,6 +65,37 @@ class TestInitKMP(unittest.TestCase):
 class TestFibbStr(unittest.TestCase):
     def test_fibb8(self):
         self.assertEqual(fibb_str(8), 'abbabbababbabbababbababbabbababbab')
+
+
+class TestFullKMP(unittest.TestCase):
+    def test_short_00(self):
+        pat, f = init_kmp('a')
+        self.assertEqual(apply_kmp(pat, f, 'bcdef'), [])
+        self.assertEqual(apply_kmp(pat, f, 'abcdef'), [0])
+        self.assertEqual(apply_kmp(pat, f, 'bacdef'), [1])
+        self.assertEqual(apply_kmp(pat, f, 'bcadef'), [2])
+        self.assertEqual(apply_kmp(pat, f, 'bcdaef'), [3])
+        self.assertEqual(apply_kmp(pat, f, 'bcdeaf'), [4])
+        self.assertEqual(apply_kmp(pat, f, 'bcdefa'), [5])
+
+    def test_short_01(self):
+        pat, f = init_kmp('a')
+        self.assertEqual(apply_kmp(pat, f, 'aabcdef'), [0, 1])
+        self.assertEqual(apply_kmp(pat, f, 'abacdef'), [0, 2])
+        self.assertEqual(apply_kmp(pat, f, 'abcadef'), [0, 3])
+        self.assertEqual(apply_kmp(pat, f, 'abcdaef'), [0, 4])
+        self.assertEqual(apply_kmp(pat, f, 'abcdeaf'), [0, 5])
+        self.assertEqual(apply_kmp(pat, f, 'abcdefa'), [0, 6])
+
+    def test_word_00(self):
+        pat, f = init_kmp('abacaba')
+        self.assertEqual(apply_kmp(pat, f, 'qwrefastrtadfas'), [])
+        self.assertEqual(apply_kmp(pat, f, 'abacabaqwrefastrtadfas'), [0])
+        self.assertEqual(apply_kmp(pat, f, 'abacabaqwrefastrtadfasabacaba'), [0, 22])
+        self.assertEqual(apply_kmp(pat, f, 'qwrefaabacabastrtadfas'), [6])
+        self.assertEqual(apply_kmp(pat, f, 'qwreabacabafastabacabartadabacabafas'), [4, 15, 26])
+        self.assertEqual(apply_kmp(pat, f, 'qwrabacabaabacabaabacabaefastrtadfas'), [])
+        
         
 if __name__ == '__main__':
     unittest.main()
