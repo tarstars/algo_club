cb5a9e031e336d7a497f88cf86b87e9a541d07f9
Arseniy Trushin <tass@thoth.travel>
2020-03-12 15:37:51 +0300

    review

--- sandbox/users/tarstars/fraction/fraction.py
+++ sandbox/users/tarstars/fraction/fraction.py
@@ -2,6 +2,65 @@ import io
 import unittest
 
 
+def frying_pan(initial_value, step, process_value):
+    """Solves problem of determination of period and non-period parts of the sequence
+    initial_value, step(initial_value), step(step(initial_value)), ...
+
+    :param initial_value: first element in the sequence
+    :param step: function that moves us toward in sequence
+    :param process_value: callable object that gathers information
+    :return:
+    """
+    a = initial_value
+
+    a = step(a)
+    b = step(a)
+
+    while a != b:
+        a = step(a)
+        b = step(step(b))
+
+    c = initial_value
+    process_value.start()
+    while b != c:
+        process_value(c)
+        c = step(c)
+        b = step(b)
+
+    process_value.begin_period()
+    process_value(c)
+
+    c = step(c)
+    while b != c:
+        process_value(c)
+        c = step(c)
+    process_value.finish()
+
+
+class StringAccumulator:
+    """Allows to accumulate results of division in form "0.<non periodic part> (<periodic part>)"
+
+    """
+    def __init__(self, digit_extractor):
+        self.buf = io.StringIO()
+        self.digit_extractor = digit_extractor
+
+    def __call__(self, val):
+        self.buf.write(str(self.digit_extractor(val)))
+
+    def start(self):
+        self.buf.write('0.')
+
+    def begin_period(self):
+        self.buf.write('(')
+
+    def finish(self):
+        self.buf.write(')')
+
+    def getvalue(self):
+        return self.buf.getvalue()
+
+
 def fraction_string(n: int) -> str:
     """For values n greater than 1 this function calculates decimal
     representation of 1/n fraction and puts it in string
@@ -9,40 +68,17 @@ def fraction_string(n: int) -> str:
     :param n: denominator of fraction
     :return: string with decimal fraction representation 0.<non periodic part> (<periodic part>)
     """
-    buf = io.StringIO()
 
-    def f(r):
+    def next_reminder(r):
         return (r * 10) % n
 
-    def d(r):
+    def current_digit(r):
         return str((r * 10) // n)
 
-    a = 1
+    string_accumulator = StringAccumulator(digit_extractor=current_digit)
+    frying_pan(initial_value=1, step=next_reminder, process_value=string_accumulator)
 
-    a = f(a)
-    b = f(a)
-
-    while a != b:
-        a = f(a)
-        b = f(f(b))
-
-    c = 1
-    buf.write('0.')
-    while b != c:
-        buf.write(d(c))
-        c = f(c)
-        b = f(f(b))
-
-    buf.write('(')
-    buf.write(d(c))
-
-    c = f(c)
-    while b != c:
-        buf.write(d(c))
-        c = f(c)
-    buf.write(')')
-
-    return buf.getvalue()
+    return string_accumulator.getvalue()
 
 
 class TestFractionString(unittest.TestCase):
@@ -50,6 +86,9 @@ class TestFractionString(unittest.TestCase):
         self.assertEqual(fraction_string(7), '0.(142857)')
 
     def test_01(self):
+        self.assertEqual(fraction_string(14), '0.0(714285)')
+
+    def test_02(self):
         self.assertEqual(fraction_string(137), '0.(00729927)')
 
 
