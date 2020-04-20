3404e06feafef88e8510f2b7ac79cf3e2c4fbf18
d.chirkina <d.chirkina@corp.mail.ru>
2020-04-21 02:49:23 +0300

    [Matrix Multiplication, C]: Resolve `Changes Requested` Commit

--- sandbox/users/dilemma/matrix_multiplication.c
+++ sandbox/users/dilemma/matrix_multiplication.c
@@ -6,26 +6,10 @@ int main() {
   int h1 = 3;
   int w1 = 5;
   int *p1 = (int*) malloc(h1 * w1 * sizeof(int));
-  /*int *p1 = memcpy(malloc(sizeof *p1 * h1 * w1),
-    (int[h1 * w1]) {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-     sizeof *p1 * h1 * w1);*/
 
   p1[0] =  11;  p1[1] = 12;  p1[2] = 13;  p1[3] = 14;  p1[4] = 15;
   p1[5] =  21;  p1[6] = 22;  p1[7] = 23;  p1[8] = 24;  p1[9] = 25;
   p1[10] = 31; p1[11] = 32; p1[12] = 33; p1[13] = 34; p1[14] = 35;
-
-  // int* p1 = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-
-  printf("first matrix is:\n");
-  for (int p = 0; p < h1; ++p) {
-    for (int q = p*w1; q < w1*(p+1); ++q) {
-      //p1[q] = 1;
-      printf("%d ", p1[q]);
-    }
-    printf("\n");
-  }
-
-  printf("\n");
 
   printf("first matrix is:\n");
   for (int p = 0; p < h1; ++p) {
@@ -47,18 +31,12 @@ int main() {
   int w2 = 2;
   int *p2 = (int*) malloc(h2 * w2 * sizeof(int));
 
-  /*p2[0] =  11;  p2[1] = 12;  p2[2] = 13;  p2[3] = 14;  p2[4] = 15;
-  p2[5] =  21;  p2[6] = 22;  p2[7] = 23;  p2[8] = 24;  p2[9] = 25;*/
-
   p2[0] =  11;  p2[1] = 12;  p2[2] = 21;  p2[3] = 22;  p2[4] = 31;
   p2[5] =  32;  p2[6] = 41;  p2[7] = 42;  p2[8] = 51;  p2[9] = 52;
-
-  // p2 = {1, 1, 1, 1, 1, 1, 1, 1};
 
   printf("second matrix is:\n");
   for (int p = 0; p < h2; ++p) {
     for (int q = 0; q < w2; ++q) {
-      //p2[p*w2 + q] = 1;
       printf("%d ", p2[p*w2 + q]);
     }
     printf("\n");
@@ -78,15 +56,8 @@ int main() {
     for (int q = 0; q < w2; ++q) {  // h2 == w1
        for (int r = 0; r < w1; r++){
             result_item += p1[p*w1 + r] * p2[r*w2 + q];
-            ///////printf("%d*%d+", p1[p*w1 + r], p2[r*w2 + q]); // p2[r*w2 + q]
-            //printf("%d ", p1[r*h1 + p]);
-            //printf("%d ", p1[p*w1 + r]);
-            //printf("%d ", p2[q*h2 + r]);
             }
-            //if(p < w2)
-            //            printf("%d ", p2[q*w2 + p]);
             printf("%d ", result_item);
-            ///////printf(" ");
             result_item = 0;
        }
             printf("\n");
